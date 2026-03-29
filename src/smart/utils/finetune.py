67dc1493f65e8c4501868f685d89a5829cbd2fd1
h-sb <h-sb@es.naverlabs.com>
2026-03-29 11:12:28 +0900

    doing

--- src/smart/utils/finetune.py
+++ src/smart/utils/finetune.py
@@ -28,7 +28,13 @@ def set_model_for_finetuning(model: torch.nn.Module, finetune: bool) -> None:
             p.requires_grad = False
 
         try:
-            _unfreeze(model.agent_encoder.flow_decoder)
-            log.info("Unfreezing flow_decoder (full HierarchicalFlowDecoder)")
+            _unfreeze(model.agent_encoder.flow_decoder.step_refiner)
+            log.info("Unfreezing flow_decoder.step_refiner")
         except Exception:
-            log.info("No flow_decoder in model.agent_encoder")
+            log.info("No flow_decoder.step_refiner in model.agent_encoder")
+
+        try:
+            _unfreeze(model.agent_encoder.flow_decoder.velocity_head)
+            log.info("Unfreezing flow_decoder.velocity_head")
+        except Exception:
+            log.info("No flow_decoder.velocity_head in model.agent_encoder")
